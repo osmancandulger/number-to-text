a336b3aa5c8f808de93560605b22a624752dfe94
Osman <osmancandulger@gmail.com>
2021-11-06 21:39:40 +0300

    Feat: Four digit converter into convertText func

--- app.ts
+++ app.ts
@@ -1,70 +1,81 @@
 const convertedNum = (num: String, digit: number) => {
-  if ((digit == 3 || digit == 2) && num == 'sıfır') {
+  if ((digit == 3 || digit == 2 || digit == 4) && num == 'sıfır') {
     return '';
   }
+
   return num;
 };
 const convertText = (number: number) => {
+  const digit: number = String(number).length;
   const splitted: string[] = String(number).split('');
   let result: string = '';
   const ones: String[] = [
-    'sıfır',
-    'bir',
-    'iki',
-    'üç',
-    'dört',
-    'beş',
-    'altı',
-    'yedi',
-    'sekiz',
-    'dokuz',
+    'Sıfır',
+    'Bir',
+    'İki',
+    'Üç',
+    'Dört',
+    'Beş',
+    'Altı',
+    'Yedi',
+    'Sekiz',
+    'Dokuz',
   ];
   const tens: String[] = [
-    'sıfır',
-    'on',
-    'yirmi',
-    'otuz',
-    'kırk',
-    'elli',
-    'altmış',
-    'yetmiş',
-    'seksen',
-    'doksan',
-    'yüz',
+    'Sıfır',
+    'On',
+    'Yirmi',
+    'Otuz',
+    'Kırk',
+    'Elli',
+    'Eltmış',
+    'Yetmiş',
+    'Seksen',
+    'Doksan',
+    'Yüz',
   ];
   const aHundreds: String[] = [
-    'yüz',
-    'ikiyüz',
-    'üçyüz',
-    'dörtyüz',
-    'beşyüz',
-    'altıyüz',
-    'yediyüz',
-    'sekizyüz',
-    'dokuzyüz',
+    'Yüz',
+    'İki yüz',
+    'Üç yüz',
+    'Dört yüz',
+    'Beş yüz',
+    'Altı yüz',
+    'Yedi yüz',
+    'Seki zyüz',
+    'Dokuz yüz',
   ];
   if (number == 0) return ones[0];
   if (String(number).length == 1) {
     return ones[number - 1];
   } else if (String(number).length == 2) {
-    const digit: number = String(number).length;
-    return (
-      convertedNum(tens[Number(splitted[0])], digit) +
-      ' ' +
-      convertedNum(ones[Number(splitted[1])], digit)
-    );
+    return (result = `${convertedNum(
+      tens[Number(splitted[0])],
+      digit,
+    )} ${convertedNum(ones[Number(splitted[1])], digit)}`);
   } else if (String(number).length == 3) {
-    const digit: number = String(number).length;
-    const splitted: string[] = String(number).split('');
     console.log(splitted);
 
-    result =
-      convertedNum(aHundreds[Number(splitted[0]) - 1], digit) +
-      '' +
-      convertedNum(tens[Number(splitted[1])], digit) +
-      '' +
-      convertedNum(ones[Number(splitted[2])], digit);
+    result = `${convertedNum(
+      aHundreds[Number(splitted[0]) - 1],
+      digit,
+    )} ${convertedNum(tens[Number(splitted[1])], digit)} ${convertedNum(
+      ones[Number(splitted[2])],
+      digit,
+    )}`;
+  } else if (String(number).length == 4) {
+    result = `${
+      convertedNum(ones[Number(splitted[0])], digit) != 'bir'
+        ? convertedNum(ones[Number(splitted[0])], digit)
+        : ''
+    } Bin ${convertedNum(
+      aHundreds[Number(splitted[1]) - 1],
+      digit,
+    )} ${convertedNum(tens[Number(splitted[2])], digit)} ${convertedNum(
+      ones[Number(splitted[3])],
+      digit,
+    )}`;
   }
   return result;
 };
-console.log(convertText(431));
+console.log(convertText(9999));
